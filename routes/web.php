f0bbcbaa918476dc0f11e143e1f8c216ee51b730
siddiqui3035 <s.h.siddiqui5830@gmail.com>
2022-03-07 12:36:05 +0600

    update readme file

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/app', function () {
+    // dd('route is working');
+    return view('app');
+});
